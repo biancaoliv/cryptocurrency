4190764d0ed595d91741df5313c4a55e3df71598
Bianca Oliveira <bianca.oliv.bn@gmail.com>
2024-11-25 18:22:30 -0300

    Initials config

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Footer from './components/Common/Footer/Footer';
+import Header from './components/Common/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
